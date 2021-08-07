e3277ddafd56688cc0dfb7dae9f5b6470205fae7
Prashant Patil <prashantpatil16012001@gmail.com>
2021-08-07 18:48:07 +0530

    first save

---
+++ Interest Form - GRIP@TSF.cpp
@@ -0,0 +1,11 @@
+Interest Form - GRIP@TSF
+Thanks! We have received your interest area. 
+
+To complete the joining formalities, please create and complete your profile on My Credible Info (https://mycredible.info/login) with Access Code: TSF-INTERN as suggested in the GRIP@TSF-2021 document (https://docs.google.com/presentation/d/1ewoYOi5YK0UifAaUnmUYsUaucDKHRpKPhNsS7vIgx5M/edit).
+
+Please make sure you read the Application Process section in the FAQs: https://docs.google.com/document/d/1EUo3Vly4C9ym6OW8tjJsbMzJUnBoFXwuSh-Pit9vNso/edit
+
+Further, please join our LinkedIn network (https://www.linkedin.com/groups/10379184/) to connect with other interns and see our internship updates. 
+
+Regards,
+GRIP@TSF
