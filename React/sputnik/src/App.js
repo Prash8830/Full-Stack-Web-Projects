6b996a5894940c28df911ce3b18b3fdaab67346a
Prashant Patil <prashantpatil16012001@gmail.com>
2021-09-15 18:31:45 +0530

    Use Prop and default prop

--- React/sputnik/src/App.js
+++ React/sputnik/src/App.js
@@ -1,40 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Progress from './components/Progress';
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid"> 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Connect
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="https://github.com/Prash8830">Github</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="https://www.linkedin.com/in/prashant-patil-8830/">Linkdin</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="https://twitter.com/Prash8830">Twitter</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      {/* used navbar.js file for importing navbar which is coded there */}
+      {/* we can use props it is basicallly like we can change the attributes of file which we loaded */}
+      {/* below i change title of navigation bar using props  */}
+      <Navbar title="Profile" num={91482293838}/>
+      {/* used buttons from progress.js file which is in components folder so like this u can use it  */}
+      <Progress />
     </>
   );
 }
