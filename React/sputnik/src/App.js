0da1ed82969b13ddbc30dc27c8756e5d14ef825c
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-16 19:25:56 +0530

    Featured Dark mode

--- React/sputnik/src/App.js
+++ React/sputnik/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Navbar from './components/Navbar' 
-import Form from './components/Form'
+import Navbar from './components/Navbar' 
+import About from './components/About' 
+// import Form from './components/Form'
 
 function App() {
   return (
     <>
-      <Form />
+      <Navbar/>
+      <About/>
     </>
   );  
 }
