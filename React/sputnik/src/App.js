f93257aabaab540d5596b400b88e825741170dcf
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-17 18:15:57 +0530

    added custom alerts with timmeout and fixed some bugs

--- React/sputnik/src/App.js
+++ React/sputnik/src/App.js
@@ -5,26 +5,40 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar' 
 // import About from './components/About' 
 import Form from './components/Form'
+import Alert from './components/Alert'
 
 function App() {
   const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
+  const showAlert = (message,type) => {
+    setalert({
+      msg:message,
+      ty:type})
+      setTimeout(()=>{
+        setalert(null);
+      },1500);
+  }
   const togglemode= () =>{
     if(mode ==='light')
     {
         setmode("dark");
         document.body.style.backgroundColor='#073642';
         // document.body.style.color='#lightgreen';
+        showAlert("Dark Mode is enabled","Success !");
     }
     else{
         setmode("light");
         document.body.style.backgroundColor='white';
         // document.body.style.Color='black';
+        showAlert("Light Mode is enabled","Success !");
+
     }
 }
   return (
     <>
       <Navbar title="Home" mode={mode} togglemode={togglemode}/>
-      <Form mode={mode}/>
+      <Alert alert={alert} />
+      <Form mode={mode} showAlert={showAlert}/>
     </>
   );  
 }
--- React/sputnik/src/components/Form.js
+++ React/sputnik/src/components/Form.js
@@ -6,21 +6,28 @@ export default function Form(props) {
         // console.log('Uppercase was clicked'+ text);
         let newtext = text.toUpperCase();
         setText(newtext);
+        props.showAlert("Converted to uppercase","Success!");
     }
     const handleLoclick = () => {
-        // console.log('Uppercase was clicked'+ text);
+        // console.log('lowercase was clicked'+ text);
         let newtext = text.toLowerCase();
         setText(newtext);
+        props.showAlert("Converted to lowercase","Success!");
+
     }
     const handleClclick = () => {
-        // console.log('Uppercase was clicked'+ text);
+        // console.log('clear was clicked'+ text);
         let newtext = "";
         setText(newtext);
+        props.showAlert("Text is clear","Warning!");
+
     }
     const handleReclick = () => {
         // console.log('Uppercase was clicked'+ text);
         let newtext = text.replace(/[ ]+/g, " ");
         setText(newtext);
+        props.showAlert("Spaces are removed","Success!");
+
     }
     const handleOnChange = (event) => {
         // console.log('On change');
@@ -36,6 +43,18 @@ export default function Form(props) {
         }
         return count;
     }
+    function countwords(data){
+        let count= 0;
+        let ar = text.split(" ");
+        for (let i=0; i<ar.length; i++) {
+            if(ar[i]!="")
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
 
     const [text, setText] = useState('your text goes here');
     // setText("Prashant");
@@ -44,7 +63,7 @@ export default function Form(props) {
             <h1 id="txtx" style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>Text Investigator For Everyone</h1>
             <div className="container" id="txtx">
                 <label htmlFor="comment" style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>Enter Text :</label>
-                <textarea className="form-control" style={{backgroundColor: props.mode === 'dark'?'#073642':'white',color: props.mode === 'dark'?'red':'#212529'}} rows="5" id="comment" value={text} onChange={handleOnChange} placeholder={text}></textarea>
+                <textarea  className="form-control" style={{backgroundColor: props.mode === 'dark'?'#073642':'white',color: props.mode === 'dark'?'red':'#212529'}} rows="5" id="comment" value={text} onChange={handleOnChange} placeholder={text}></textarea>
             </div>
             <button className="btn btn-primary" id="txtxg" onClick={handleUpclick}>To Upper Case</button>
             <button className="btn btn-primary" id="txtxg" onClick={handleLoclick}>To Lower Case</button>
@@ -52,9 +71,9 @@ export default function Form(props) {
             <button className="btn btn-primary" id="txtxg" onClick={handleReclick}>Remove Space</button>
             <div id="txtx">
                 <h1 style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>Text Analysis :</h1>
-                <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>1) No. of Words : {text.length===0?0:text.split(" ").length}</p>
+                <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>1) No. of Words : {countwords(text)}</p>
                 <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>2) No. of Characters : {text.length}</p>
-                <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>3) Minutes Required To Read: {text.length===0?0:0.008 * text.split(" ").length}</p>
+                <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>3) Time Required To Read to human: {text.length===0?0:0.008 * countwords(text)} minutes</p>
                 <p style={{color: props.mode === 'dark'?'lightgreen':'#212529'}}>4) No. of Words starting with Vowel: {getvowelcount(text)}</p>
             </div>
         </>
