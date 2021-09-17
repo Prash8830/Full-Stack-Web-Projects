bb40a5ef0bb6993e457fb110df0874759e35a523
Prashant Rohidas Patil <prashantpatil16012001@gmail.com>
2021-09-17 22:15:58 +0530

    use react router

--- React/sputnik/src/components/Navbar.js
+++ React/sputnik/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
@@ -12,9 +13,12 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">{props.title}</a>
+                                <Link className="nav-link active" aria-current="page" to="/">{props.title}</Link>
                             </li>
 
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/about">About</Link>
+                            </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Connect
